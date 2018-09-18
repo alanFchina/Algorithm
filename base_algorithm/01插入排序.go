a1a9aa0dfb9181c1fae72a66f13751c1f3fec8e5
alan <962899122@qq.com>
2018-09-18 14:44:31 +0800

    插入排序增加注释

--- base_algorithm/01插入排序.go
+++ base_algorithm/01插入排序.go
@@ -1,5 +1,11 @@
 package base_algorithm
 
+// 插入排序是稳定的原址排序, 但是对于不同的输入, 时间复杂度不同
+// f(n) = O(n²); f(n) = Ω(n)
+
+// 使用二分法对插入排序进行优化:
+//     不可行, 因为移动元素所花费的时间省不掉
+
 func InsertSort(arr []int) {
 	for i := 1; i < len(arr); i++ {
 		key := arr[i]
