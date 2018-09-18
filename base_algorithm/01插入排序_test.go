03c7dde66a44f38f190338b188e18074a104eadd
alan <962899122@qq.com>
2018-09-18 16:29:24 +0800

    分离测试数据

--- base_algorithm/01插入排序_test.go
+++ base_algorithm/01插入排序_test.go
@@ -2,16 +2,6 @@ package base_algorithm
 
 import "testing"
 
-var cases = []struct {
-		input  []int
-		wanted []int
-	}{
-		{[]int{1, 2, 3, 4, 5}, []int{1, 2, 3, 4, 5}},
-		{[]int{}, []int{}},
-		{[]int{9, 8, 7, 6, 5, 4, 3, 2, 1}, []int{1, 2, 3, 4, 5, 6, 7, 8, 9}},
-		{[]int{1, 5, 3, 6, 12, 8, 9, 32, 6, 0, 1, 4, 4, 5, 4,}, []int{0, 1, 1, 3, 4, 4, 4, 5, 5, 6, 6, 8, 9, 12, 32}},
-	}
-
 func TestInsertSort(t *testing.T) {
 	for i, v := range cases {
 		if InsertSort(v.input); !equal(v.input, v.wanted) {
